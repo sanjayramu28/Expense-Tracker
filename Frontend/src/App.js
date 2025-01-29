bc156978739d34c00826c0df3e85d0a2e5840b0e
Sanjay <email@example.com>
2025-01-29 20:38:12 +0530

    Getting Ready for deploy

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,6 +5,8 @@ import AddExpense from "./Components/AddExpense/AddExpense";
 import RemoveExpense from './Components/RemoveExpense/RemoveExpense'
 import Auth from "./Components/Authentication/Login";
 import Register from "./Components/Authentication/Register";
+const apiUrl = process.env.REACT_APP_FRONTEND_URL_PRODUCTION || "http://localhost:5000";
+
 
 
 const App = () => {
