aaf8e2d24f17f932bcb6b9d15120d019ea490b04
Sanjay <email@example.com>
2025-01-29 22:08:00 +0530

    Getting Ready for deploy

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,10 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Home from './Components/Home/Home'
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom"
 import AddExpense from "./Components/AddExpense/AddExpense";
 import RemoveExpense from './Components/RemoveExpense/RemoveExpense'
 import Auth from "./Components/Authentication/Login";
 import Register from "./Components/Authentication/Register";
-const apiUrl = process.env.REACT_APP_FRONTEND_URL_PRODUCTION || "http://localhost:5000";
 
 
 
@@ -13,9 +12,9 @@ const App = () => {
 
   return (
     <>
-<BrowserRouter>
+<HashRouter>
       <Appf />
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
